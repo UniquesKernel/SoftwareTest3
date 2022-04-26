5392260b69cb3349ac3212ddd23066dd5a0489cf
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2022-04-26 17:03:41 +0200

    Create AdjustableTimer.cs
    Is meant to be a more specialized instance of Timer

---
+++ src/Microwave.Classes/Boundary/AdjustableTimer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microwave.Classes.Boundary
+{
+    public class AdjustableTimer : Timer
+    {
+        public AdjustableTimer() : base() {}
+
+        public void AdjustTimer(int sec)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
