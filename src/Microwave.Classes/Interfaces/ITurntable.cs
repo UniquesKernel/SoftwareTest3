b7ad765311b50676e22ee08c08753ed3aa838c15
Yevhen <yevhen89@gmail.com>
2022-04-26 23:16:20 +0200

    Basic-Turntable

---
+++ src/Microwave.Classes/Interfaces/ITurntable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Interfaces
+{
+    internal interface ITurntable
+    {
+        public void Start();
+        public void Stop();
+        public void setSpeed(int speed);
+    }
+}
