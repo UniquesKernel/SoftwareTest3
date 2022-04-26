77a838a6927044c007cd2c176fccfbddc8f06d83
Yevhen <yevhen89@gmail.com>
2022-04-26 23:16:20 +0200

    Basic-Turntable

---
+++ src/Microwave.Classes/Boundary/Turntable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Interfaces;
+
+namespace Microwave.Classes.Boundary
+{
+    internal class Turntable : ITurntable
+    {
+        private int speed;
+
+        public Turntable()
+        {
+            setSpeed();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+
+        //speed from 1 to 100
+        public void setSpeed(int speed = 50)
+        {
+            if (speed < 1)
+            {
+                this.speed = 1;
+                return;
+            }
+            if(speed > 100)
+            {
+                this.speed = 100;
+                return;
+            }
+            this.speed = speed;
+        }
+
+    }
+}
